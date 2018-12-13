9a3886e970a7e4aa384177aa94cf257043617fd9
lizejuan <13313571683@163.com>
2018-12-13 10:53:46 +0800

    第一次提交

---
+++ src/main/java/com/tt/data7/Data7Application.java
@@ -0,0 +1,14 @@
+package com.tt.data7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data7Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tt/data7/Data7ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tt.data7;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Data7ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
